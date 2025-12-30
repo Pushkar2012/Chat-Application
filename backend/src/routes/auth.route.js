8867e35d97e833dacc930eff726204fb2cfaa1ad
Pushkar Parmar <p.parmar2786@gmail.com>
2025-12-30 18:43:14 +0530

    remove unnecessary blank lines in auth route file

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -7,13 +7,9 @@ import { checkAuth } from '../controller/auth.con.js';
 const router = express.Router();
 
 router.post("/signup", signup);
-
 router.post("/login", login);
-
 router.post("/logout", logout);
-
 router.put("/update-profile", protectRoute, updateProfile);
-
 router.get("/check",protectRoute,checkAuth);
 
 export default router;
